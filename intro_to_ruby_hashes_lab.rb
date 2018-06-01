11b43f7e2ca9dd34f799cb3879df3ecfd4db7e59
Richard Fenix <richard.fenix@gmail.com>
2018-06-01 14:06:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,17 +42,40 @@ def monopoly_with_third_tier
   monopoly		
 end
 
-########################
-
-
-
-########################
-
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	monopoly = {}
+  monopoly[:railroads] = {}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:names][:reading_railroad] = {}
+
+  monopoly[:railroads][:names][:reading_railroad]['mortgage_value'] = '$100'
+  
+  
+  monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+  monopoly[:railroads][:names][:pennsylvania_railroad]['mortgage_value'] = '$200'
+  
+  
+  monopoly[:railroads][:names][:b_and_o_railroad] = {}
+  monopoly[:railroads][:names][:shortline] = {}
+
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+
+  monopoly			
 	
 end
 
+
+      it "sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is '$200'" do
+
+        expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:pennsylvania_railroad)).to be true
+        expect(monopoly_with_fourth_tier.values[0][:names].values[1].has_key?("mortgage_value")).to be true
+        expect(monopoly_with_fourth_tier.values[0][:names].values[1].values[0]).to eq("$200")
 
 
 
