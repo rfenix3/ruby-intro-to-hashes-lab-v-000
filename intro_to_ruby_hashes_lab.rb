6a82daa2d1e8f56d3e86adcdaa2a3f120ea591eb
Richard Fenix <richard.fenix@gmail.com>
2018-06-01 13:51:24 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,9 +17,10 @@ def monopoly_with_second_tier
 	monopoly = {}
   monopoly[:railroads] = {}
   monopoly[:railroads][:pieces]= 4
+  monopoly[:railroads][:names]= {}
+  monopoly[:railroads][:rent_in_dollars]= {}
   monopoly
-#  monopoly[:railroads][:names]= {}
-#  monopoly[:railroads][:rent_in_dollars]= {}
+
 end
 
 
