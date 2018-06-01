592cf9659199d78520796b8fb5bca9801a1bc4ca
Richard Fenix <richard.fenix@gmail.com>
2018-06-01 13:59:31 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,6 +28,10 @@ def monopoly_with_third_tier
   monopoly[:railroads] = {}
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:names][:reading_railroad] = {}
+  monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+
+  
   monopoly[:railroads][:rent_in_dollars] = {}
   monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
@@ -38,28 +42,8 @@ end
 
 ########################
 
-      it "sets the 4th key of the :rent_in_dollars hash to a symbol, :four_pieces_owned, whose value is the integer 200" do
 
-        expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-        expect(monopoly_with_third_tier.values[0][:rent_in_dollars].keys.count).to eq(4)
-        expect(monopoly_with_third_tier.values[0][:rent_in_dollars][:four_pieces_owned]).to eq(200)
-      end
 
-      it "sets the 1st key of the :names hash to a symbol, :reading_railroad, whose value is an empty hash" do
-
-        expect(monopoly_with_third_tier.keys.count).to eq(1)
-        expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-        expect(monopoly_with_third_tier.values[0][:names].keys[0]).to eq(:reading_railroad)
-        expect(monopoly_with_third_tier.values[0][:names].values[0]).to eq({})
-      end
-
-      it "sets the 2nd key of the :names hash to a symbol, :pennsylvania_railroad, whose value is an empty hash" do
-
-        expect(monopoly_with_third_tier.keys.count).to eq(1)
-        expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-        expect(monopoly_with_third_tier.values[0][:names].keys[1]).to eq(:pennsylvania_railroad)
-        expect(monopoly_with_third_tier.values[0][:names].values[1]).to eq({})
-      end
 
       it "sets the 3rd key of the :names hash to a symbol, :b_and_o_railroad, whose value is an empty hash" do
 
